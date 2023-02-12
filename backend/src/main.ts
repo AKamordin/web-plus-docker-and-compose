a3980b1891cd0c407489d9da3b089f3389e179f9
Alexander Kamordin <akamordin@yandex-team.ru>
2023-02-12 19:18:59 +0300

    Init 5

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {origin: ['http://kpd.akamodin.nomoredomains.work', 'https://kpd.akamodin.nomoredomains.work']}
+  });
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
-  const origins = configService.get<string>('ALLOWED_ORIGINS').split(',') || '*';
-  app.enableCors({origin: origins});
+  //const origins = configService.get<string>('ALLOWED_ORIGINS').split(',') || '*';
+  //app.enableCors({origin: origins});
   const PORT = configService.get<number>('PORT') || DEFAULT_PORT;
   await app.listen(PORT);
 }
