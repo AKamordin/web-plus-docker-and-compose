158d88e777b9fbfa92592609529aa9ae2ee244b9
Alexander Kamordin <akamordin@yandex-team.ru>
2023-02-12 21:21:04 +0300

    Init 15

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       preflightContinue: false,
       optionsSuccessStatus: 204,
+      allowedHeaders: ['content-type'],
     },
   });
   app.useGlobalPipes(new ValidationPipe());
