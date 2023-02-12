57878ad6756291e863837acfcbda5ed4c6a83f2d
Alexander Kamordin <akamordin@yandex-team.ru>
2023-02-12 20:38:31 +0300

    Init 10

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 import configuration from './config/configuration';
 
 async function bootstrap() {
-  console.log(configuration().allowedOrigins);
   const app = await NestFactory.create(AppModule, {
-    cors: {origin: ['http://kpd.akamodin.nomoredomains.work', 'https://kpd.akamodin.nomoredomains.work']}
+    cors: {origin: configuration().allowedOrigins}
   });
   app.useGlobalPipes(new ValidationPipe());
   const PORT = configuration().port;
